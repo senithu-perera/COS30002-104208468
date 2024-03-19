1d5cbf8a8fc11d01e825d0cb605bc985f7a4db80
SDPSTUDIOS <103296977+SDPSTUDIOS@users.noreply.github.com>
2024-03-19 15:48:27 +1100

    FInite State Mechine update

--- 02 - Lab - FSM and Python/Submission/FSM.py
+++ 02 - Lab - FSM and Python/Submission/FSM.py
@@ -1,20 +1,31 @@
 class GameCharacter:
     def __init__(self):
         self.state = "Chasing"
+        self.enemy_Distance = 57.5;
+        self.enemy_Health = 100;
+        self.enemy_Attackrate = 10;
 
     def attack(self):
-        if self.state == "Chasing":
+        if self.state == "Chasing" and self.enemy_Distance <= 10:
             print("Attacking!")
             self.state = "Attacking"
+            player_health -= self.enemy_Attackrate
         else:
             print("Already in attacking state")
 
     def chase(self):
-        if self.state == "Attacking":
+        if self.state == "Attacking" and self.enemy_Distance > 10:
             print("Switching to chasing mode")
             self.state = "Chasing"
         else:
             print("Already in chasing state")
+
+    def Stand(self):
+        if self.state == "Chasing" and self.enemy_Distance > 50:
+            print("Switching to Standing mode")
+            self.state = "Stand"
+        else:
+            print("Already in Standing state")
 
     def display_state(self):
         print(f"Character is currently {self.state}")
@@ -28,3 +39,4 @@ game_character.attack()
 game_character.display_state()
 game_character.chase()
 game_character.display_state()
+game_character.Stand()
